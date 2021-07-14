4ff0a226bfe4c13a205e5fd2140e6e68ce8dfe7b
Grzegorz Tomicki <gtomik@gmail.com>
2021-07-14 11:49:34 +0200

    hex, hsl, rgb

--- colors.js
+++ colors.js
@@ -78,6 +78,68 @@ function removeStyleFromSpan(event) {
   });
 }
 
+function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
+}
+
+function hexToRGB(h) {
+  let r = 0, g = 0, b = 0;
+
+  if (h.length == 4) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+
+  } else if (h.length == 7) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+  }
+
+  return "rgb(" + +r + "," + +g + "," + +b + ")";
+}
+
 function showAllColors(labels) {
 
   // set default color to red
@@ -127,7 +189,7 @@ document.addEventListener('click', function (e) {
 
     clearTimeout(timeoutID);
 
-    const color = e.target.textContent.trim();
+    const hex = e.target.textContent.trim();
     const input = document.createElement('input');
     const hiddenColor = document.querySelector('.hidden-color');
 
@@ -138,9 +200,12 @@ document.addEventListener('click', function (e) {
 
     document.body.appendChild(input);
 
-    info.innerHTML = `${color} <br />Code copied to clipboard.`
+    const hsl = hexToHSL(hex);
+    const rgb = hexToRGB(hex);
 
-    input.setAttribute('value', color);
+    info.innerHTML = `${hex} <br /> ${hsl} <br /> ${rgb} <br />Code copied to clipboard.`
+
+    input.setAttribute('value', `${hex}, ${hsl}, ${rgb}`);
     input.select();
     document.execCommand('copy');
 

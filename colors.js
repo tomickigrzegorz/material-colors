35d8685e0e616cd6e6282b83a67d68a9490da525
Grzegorz Tomicki <gtomik@gmail.com>
2022-06-05 21:53:11 +0200

    feat: select color var

--- colors.js
+++ colors.js
@@ -1,7 +1,7 @@
-const root = document.querySelector('.app');
+const root = document.querySelector(".app");
 
-const div = document.createElement('div');
-div.className = 'colors flex';
+const div = document.createElement("div");
+div.className = "colors flex";
 
 div.innerHTML = `
   <div class="color__column">
@@ -12,22 +12,22 @@ div.innerHTML = `
   </div>
 `;
 
-root.insertAdjacentElement('beforeend', div);
+root.insertAdjacentElement("beforeend", div);
 
-const info = document.createElement('div');
-info.className += 'info';
+const info = document.createElement("div");
+info.className += "info";
 
-root.insertAdjacentElement('afterend', info);
+root.insertAdjacentElement("afterend", info);
 
-const topInfo = document.createElement('div');
-topInfo.className = 'top-info';
-topInfo.textContent = 'Clicking on the color bar will copy it to the clipboard';
+const topInfo = document.createElement("div");
+topInfo.className = "top-info";
+topInfo.textContent = "Clicking on the color bar will copy it to the clipboard";
 
-root.insertAdjacentElement('afterbegin', topInfo);
+root.insertAdjacentElement("afterbegin", topInfo);
 
 async function fetchColors() {
   try {
-    const response = await fetch('./colors.json');
+    const response = await fetch("./colors.json");
     const data = await response.json();
     return data;
   } catch (err) {
@@ -48,30 +48,27 @@ function generateColors(data) {
 }
 
 function setChosenColors(labels) {
-  const chosenColors = document.querySelector('.color__choose');
+  const chosenColors = document.querySelector(".color__choose");
 
   labels.reverse().map((label) => {
     const chosen = `<span class="color-item">${label[0]}</span>`;
 
-    chosenColors.insertAdjacentHTML('afterbegin', chosen);
+    chosenColors.insertAdjacentHTML("afterbegin", chosen);
   });
 
   setColors(labels);
-
-  // first red set checked
-  // document.querySelector('[title="red"]').firstElementChild.click();
 }
 
 function removeStyleFromSpan(event) {
   const target = event.target.nextElementSibling;
-  const colorItems = document.querySelectorAll('.color-item');
+  const colorItems = document.querySelectorAll(".color-item");
 
   colorItems.forEach((item) => {
-    const colorHex = item.getAttribute('data-hex');
-    const colorHexTarget = target.getAttribute('data-hex');
-    item.setAttribute('style', `background-color: ${colorHex}`);
+    const colorHex = item.getAttribute("data-hex");
+    const colorHexTarget = target.getAttribute("data-hex");
+    item.setAttribute("style", `background-color: ${colorHex}`);
     target.setAttribute(
-      'style',
+      "style",
       `transform: scale(0.5); box-shadow: 0 0 0 10px rgb(255 255 255), 0 0 0 15px ${colorHexTarget}; background-color: ${colorHexTarget}`
     );
   });
@@ -83,13 +80,13 @@ function hexToHSL(H) {
     g = 0,
     b = 0;
   if (H.length == 4) {
-    r = '0x' + H[1] + H[1];
-    g = '0x' + H[2] + H[2];
-    b = '0x' + H[3] + H[3];
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
   } else if (H.length == 7) {
-    r = '0x' + H[1] + H[2];
-    g = '0x' + H[3] + H[4];
-    b = '0x' + H[5] + H[6];
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
   }
   // Then to HSL
   r /= 255;
@@ -116,7 +113,7 @@ function hexToHSL(H) {
   s = +(s * 100).toFixed(1);
   l = +(l * 100).toFixed(1);
 
-  return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+  return "hsl(" + h + "," + s + "%," + l + "%)";
 }
 
 function hexToRGB(h) {
@@ -125,57 +122,54 @@ function hexToRGB(h) {
     b = 0;
 
   if (h.length == 4) {
-    r = '0x' + h[1] + h[1];
-    g = '0x' + h[2] + h[2];
-    b = '0x' + h[3] + h[3];
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
   } else if (h.length == 7) {
-    r = '0x' + h[1] + h[2];
-    g = '0x' + h[3] + h[4];
-    b = '0x' + h[5] + h[6];
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
   }
 
-  return 'rgb(' + +r + ',' + +g + ',' + +b + ')';
+  return "rgb(" + +r + "," + +g + "," + +b + ")";
 }
 
 function showAllColors(labels) {
-  // set default color to red
-  // setColors(labels, 'red');
-
   // set color when click on radio color
-  document.addEventListener('change', (e) => {
+  document.addEventListener("change", (e) => {
     setColors(labels, e.target.value);
-    document.documentElement.style.setProperty('--info-color', 'black');
+    document.documentElement.style.setProperty("--info-color", "black");
 
     removeStyleFromSpan(e);
   });
 }
 
 function setColors(labels) {
-  const colorList = document.querySelector('.color__list');
+  const colorList = document.querySelector(".color__list");
   // remove children div from color__list
-  colorList.innerHTML = '';
+  colorList.innerHTML = "";
 
   // remove style from body
-  document.body.removeAttribute('style');
+  document.body.removeAttribute("style");
 
   // update title
-  document.title = 'Material Colors';
-
-  const arrayColors = labels.filter((item) => item).map((item) => item[1]);
+  document.title = "Material Colors";
 
   let i = 0;
-  arrayColors.map((color, index) => {
+  labels.map((item, index) => {
+    const [colorName, color] = item;
+    // console.log(colorName, color, index);
     let column;
     if (index == i) {
-      column = document.createElement('div');
+      column = document.createElement("div");
       column.className = `column-item`;
-      colorList.insertAdjacentElement('afterbegin', column);
+      colorList.insertAdjacentElement("afterbegin", column);
     }
-    // console.log('c', color);
+
     for (const [key, value] of Object.entries(color)) {
       column.insertAdjacentHTML(
-        'beforeend',
-        `<div class="colors-item" data-type="${key}" data-color="${
+        "beforeend",
+        `<div class="colors-item" data-color-name=${colorName} data-type="${key}" data-color="${
           value.hex
         }" style="background-color: ${value.hex}; color: ${invertHex(
           value.hex
@@ -190,36 +184,38 @@ function setColors(labels) {
 }
 
 function invertHex(hexcolor) {
-  hexcolor = hexcolor.replace('#', '');
+  hexcolor = hexcolor.replace("#", "");
   const r = parseInt(hexcolor.substr(0, 2), 16);
   const g = parseInt(hexcolor.substr(2, 2), 16);
   const b = parseInt(hexcolor.substr(4, 2), 16);
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-  return yiq >= 128 ? 'black' : 'white';
+  return yiq >= 128 ? "black" : "white";
 }
 
 let timeoutID;
 
-document.addEventListener('click', function (e) {
-  if (e.target.className === 'colors-item') {
+document.addEventListener("click", function (e) {
+  const target = e.target;
+  if (e.target.className === "colors-item") {
     clearTimeout(timeoutID);
 
-    const hex = e.target.lastElementChild.textContent.trim();
-    const input = document.createElement('input');
-    const hiddenColor = document.querySelector('.hidden-color');
+    const hex = target.lastElementChild.textContent.trim();
+    const input = document.createElement("input");
+    const hiddenColor = document.querySelector(".hidden-color");
 
     if (hiddenColor) {
       hiddenColor.remove();
     }
-    input.className = 'hidden-color';
+    input.className = "hidden-color";
 
     document.body.appendChild(input);
 
     const hsl = hexToHSL(hex);
     const rgb = hexToRGB(hex);
+    const cssvar = `--color-${target.dataset.colorName}-${target.dataset.type}: ${target.dataset.color};`;
 
     let colorType = 0;
-    const selectChose = document.querySelector('.select-chose').selectedIndex;
+    const selectChose = document.querySelector(".select-chose").selectedIndex;
     switch (selectChose) {
       case 0:
         colorType = hex;
@@ -230,6 +226,9 @@ document.addEventListener('click', function (e) {
       case 2:
         colorType = hsl;
         break;
+      case 3:
+        colorType = cssvar;
+        break;
       default:
         colorType = hex;
         break;
@@ -237,27 +236,35 @@ document.addEventListener('click', function (e) {
 
     info.innerHTML = `Code copied to clipboard.<br/>${colorType}`;
 
-    input.setAttribute('value', `${colorType}`);
-    input.select();
-    document.execCommand('copy');
+    input.setAttribute("value", `${colorType}`);
+    input.classList.add("temp-input");
 
-    info.classList.add('active');
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(input.value).then(() => {
+        const tempInput = select(".tmp-input");
+        tempInput.remove();
+      });
+    } else {
+      console.log("Clipboard API not supported");
+    }
+
+    info.classList.add("active");
 
     timeoutID = setTimeout(() => {
-      info.classList.remove('active');
+      info.classList.remove("active");
     }, 1000);
 
-    const hexColor = e.target.dataset.color;
+    const hexColor = target.dataset.color;
 
-    document.documentElement.style.setProperty('--body-color', hexColor);
+    document.documentElement.style.setProperty("--body-color", hexColor);
     document.documentElement.style.setProperty(
-      '--info-color',
+      "--info-color",
       `${invertHex(hexColor)}`
     );
   }
 });
 
-const colors = document.querySelector('.colors');
+const colors = document.querySelector(".colors");
 function changeTypeOfColor() {
   const selectChose = `
     <div class="flex chose-color">
@@ -266,11 +273,12 @@ function changeTypeOfColor() {
         <option value="hex"> HEX </option>
         <option value="rgb"> RGB </option>
         <option value="hsl"> HSL </option>
+        <option value="cssvar"> VAR </option>
       </select>
     </div>
   `;
 
-  colors.insertAdjacentHTML('beforeend', selectChose);
+  colors.insertAdjacentHTML("beforeend", selectChose);
 }
 
 changeTypeOfColor();
